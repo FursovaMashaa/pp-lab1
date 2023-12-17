0595dc4949540c3b009136d6abfb99f4e30f5c30
FursovaMashaa <fursovamaria1@gmail.com>
2023-12-17 21:07:03 +0400

    Opening the yandex images and closing it after 3 secondsOpening the file and closing it after 3 seconds

---
+++ parsing.py
@@ -0,0 +1,14 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+url = "https://yandex.ru/images/"
+
+driver.get(url=url)
+time.sleep(3)
+
+driver.close()
+driver.quit()
