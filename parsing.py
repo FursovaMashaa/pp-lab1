0ca14ff881e6ebe666a792d2a65396563cf39c43
FursovaMashaa <fursovamaria1@gmail.com>
2023-12-17 23:47:11 +0400

    creating a folders

--- parsing.py
+++ parsing.py
@@ -1,7 +1,12 @@
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+
+def creating_a_folders(name):
+    if not os.path.isdir(name):
+         os.mkdir(name)
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
